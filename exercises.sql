67c5643c5447310d4f8c1cab2475c03d6013607d
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-21 17:53:48 -0700

    sql

--- exercises.sql
+++ exercises.sql
@@ -156,3 +156,152 @@ FROM
     FROM tutorial.billboard_top_100_year_end
     WHERE (year >= 1990 AND year_rank = 1) OR year_rank = 2 OR year_rank = 3
     ORDER BY year_rank, year) AS T
+
+-- Optional: What is the highest position ever reached by Phil Collins?
+
+SELECT MIN(year_rank)
+FROM tutorial.billboard_top_100_year_end
+WHERE artist = 'Phil Collins'
+
+-- What is the average position reached by Michael Jackson?
+
+SELECT AVG(year_rank)
+FROM tutorial.billboard_top_100_year_end
+WHERE artist = 'Michael Jackson'
+
+-- Madonna's average position when she actually reached the top 10
+
+SELECT AVG(year_rank)
+FROM tutorial.billboard_top_100_year_end
+WHERE artist = 'Madonna' AND year_rank <= 10
+
+-- List the top 10 artists based on their number of appearances on this list (and what that number is) since 1985
+
+SELECT artist
+FROM tutorial.billboard_top_100_year_end
+WHERE year >= 1985 AND artist IS NOT NULL
+GROUP BY artist
+ORDER BY count(artist)
+LIMIT 10
+
+-- The total count of top 10 hits written by either Elvis, Madonna, the Beatles, or Elton John
+
+SELECT count(artist)
+FROM tutorial.billboard_top_100_year_end
+WHERE artist IN('Elvis%', 'Madonna', 'Elton John', 'Beatles')
+
+ -- aapl_historical_stock_price; The count of days when Apple traded in a range that was larger than $5
+
+SELECT count(date)
+FROM tutorial.aapl_historical_stock_price
+WHERE (high - low) > 5
+
+-- The highest daily trading range that Apple stock achieved in 2012
+
+SELECT MAX(high - low)
+FROM tutorial.aapl_historical_stock_price
+WHERE year = 2012
+
+-- The average price on days when Apple's trading volume exceeded 10,000,000 shares.
+
+SELECT (high + low)/2 AS average
+FROM tutorial.aapl_historical_stock_price
+WHERE volume > 10000000
+
+-- The number of trading days in each month of the year 1992
+
+SELECT month, count(date)
+FROM tutorial.aapl_historical_stock_price
+WHERE year = 2002
+GROUP BY month
+ORDER BY month
+
+-- The maximum price Apple traded at during each year of the data set
+
+SELECT year, max(high)
+FROM tutorial.aapl_historical_stock_price
+GROUP BY year
+ORDER BY year
+
+-- The average price and trading volume on each calendar month across the full data set (this should return only 12 rows, one for each month!)
+
+SELECT month, (avg(high) + avg(low))/2 as average_price, avg(volume) as average_volume
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month
+ORDER BY month
+
+-- The average price for each month and year of data since 2008, ordered by years descending and months ascending.
+
+SELECT year, month, (avg(high) + avg(low))/2 as average_price, avg(volume) as average_volume
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month, year
+ORDER BY year DESC, month
+
+-- The average price on all days with a trading volume above 25,000,000 shares (just 1 row)
+
+SELECT(avg(high) + avg(low))/2 as average_price
+FROM tutorial.aapl_historical_stock_price
+WHERE volume > 25000000
+
+-- The average price on all months with an average daily trading volume above 10,000,000 shares.
+
+SELECT month, (avg(high) + avg(low))/2 as average_price
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month
+HAVING avg(volume) > 10000000
+ORDER BY month
+
+-- The lowest and highest prices that Apple stock achieved between 2005 and 2010 (inclusive).
+
+SELECT min(low), max(high)
+FROM tutorial.aapl_historical_stock_price
+WHERE year BETWEEN 2005 AND 2010
+
+-- The average daily trading range in months where the stock moved more than $25 (open to close)
+
+SELECT year, month, avg(high) - avg(low)
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month, year
+HAVING  (max(close) - min(open)) > 25
+ORDER BY year, month
+
+-- All months in the second half of the year where average daily trading volume was below 10,000,000.
+
+SELECT month, year
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month, year
+HAVING month > 6 AND avg(volume) < 10000000
+ORDER BY year, month
+
+-- A list of all calendar months by average daily trading volume (so only 12 rows), sorted from highest to lowest.
+
+SELECT month, avg(volume)
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month
+ORDER BY avg(volume) DESC
+
+-- Count how many unique months there are in the data set
+
+SElECT COUNT (*)
+FROM
+(SELECT year, month
+FROM tutorial.aapl_historical_stock_price
+GROUP BY month, year) AS sub
+
+-- Count how many unique years there are in the data set
+
+SElECT COUNT (distinct year)
+FROM tutorial.aapl_historical_stock_price
+
+-- Count how many unique prices there are in the data set
+
+SELECT count(price) FROM
+(
+SElECT open FROM tutorial.aapl_historical_stock_price
+UNION
+SElECT close FROM tutorial.aapl_historical_stock_price
+UNION
+SElECT high FROM tutorial.aapl_historical_stock_price
+UNION
+SElECT low FROM tutorial.aapl_historical_stock_price
+) as price
